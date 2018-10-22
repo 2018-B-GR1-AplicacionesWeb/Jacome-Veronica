5cba3b40d52ae8b8b5bed127e2cb0cb938056bc1
14fercha20 <fercha26_14_10@hotmail.com>
2018-10-22 10:05:02 -0500

    uso de variables

--- 04-operadores.js
+++ 04-operadores.js
@@ -5,10 +5,38 @@ var adrian = {
         return 'trabajo';
     }
 };
+console.log(typeof ejemplo); //tipo de dato
+console.log(ejemplo);//definicion de la funcion
+console.log(ejemplo ());// ejecucion funcion
+
+var variableUno;
+let variableDos= 2; //usar mutable
+varableDos = variableDos + 1;
+const pi = 3.1416;
+
+const vicente = {
+    nombre:  'Vicente'
+};
+vicente.nombre = 'Adrian';
+vicente.edad=24;
 
 var arregloDeNombres =['A', 'b', 'c'];
 arregloDeNombres.forEach(
-    function () {
-        
+    function (valorActual, indiceActual, arreglo) {
+
+        console.log('valorActual', valorActual);
+        console.log('indiceActual', indiceActual);
+
     }
-)
+);
+
+arregloDeNombres.forEach(
+    function (valorActual, indiceActual, arreglo) => {
+
+        console.log('valorActual', valorActual);
+        console.log('indiceActual', indiceActual);
+
+    }
+);
+arregloDeNombresDos = [ ]
+
