853790e4fa9586168527519d153091a4b227b257
14fercha20 <fercha26_14_10@hotmail.com>
2018-10-22 09:32:54 -0500

    funciones anonimas

---
+++ 04-operadores.js
@@ -0,0 +1,14 @@
+function ejemplo() {}
+    var ejemploDos = function () { } //funciones anonimas
+var adrian = {
+    trabajar: function () {
+        return 'trabajo';
+    }
+};
+
+var arregloDeNombres =['A', 'b', 'c'];
+arregloDeNombres.forEach(
+    function () {
+        
+    }
+)
